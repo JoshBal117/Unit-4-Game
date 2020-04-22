96342d6869c84104f00c0cb01e79a7c5b8b99295
Joshua Balao <joshuacbalao@gmail.com>
2020-04-21 23:12:15 -0400

    change quotations for PCs name from double quotes to single

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ $(document).ready(function) {
 //This is a list of PCs for the game (Playable Character)
 
 let characters = [
-    "Luke Skywalker": {
+    'Luke Skywalker': {
         name: 'Luke Skywalker',
         health: 100,
         attack: 5,
@@ -13,23 +13,21 @@ let characters = [
         enemyCounterAttack: 5
     } 
 
-    "Obi-Wan Kenobi":{
+    'Obi-Wan Kenobi':{
         name: 'Obi-Wan Kenobi',
         health: 120,
         attack: 8,
         imageUrl: ,
         enemyCounterAttack: 10
     }
-
-    "Darth Sidious":{
+    'Darth Sidious':{
         name: 'Darth Sidious',
         health: 150,
         attack: 12,
         imageUrl: ,
         enemyCounterAttack: 15
     }
-
-    "Darth Maul": {
+    'Darth Maul': {
         name: 'Darth Maul',
         health: 180,
         attack: 10
