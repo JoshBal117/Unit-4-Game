340beaab113e34c16a9d4ae3c374905a065c2991
Joshua Balao <joshuacbalao@gmail.com>
2020-04-21 23:03:05 -0400

    add global vairables to game.js file and create PC and Adversary markers

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+//these are the global variables
+$(document).ready(function) {
+
+
+//This is a list of PCs for the game (Playable Character)
+
+let characters = [
+    "Luke Skywalker": {
+        name: 'Luke Skywalker',
+        health: 100,
+        attack: 5,
+        imageUrl: ,
+        enemyCounterAttack: 5
+    } 
+
+    "Obi-Wan Kenobi":{
+        name: 'Obi-Wan Kenobi',
+        health: 120,
+        attack: 8,
+        imageUrl: ,
+        enemyCounterAttack: 10
+    }
+
+    "Darth Sidious":{
+        name: 'Darth Sidious',
+        health: 150,
+        attack: 12,
+        imageUrl: ,
+        enemyCounterAttack: 15
+    }
+
+    "Darth Maul": {
+        name: 'Darth Maul',
+        health: 180,
+        attack: 10
+        imageUrl: , 
+        enemyCounterAttack: 25,
+    }
+];
+
+
+
+
+
+}
