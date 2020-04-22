643c92f37f0a35c5f58341c7063bdcaf472a6f73
Joshua Balao <joshuacbalao@gmail.com>
2020-04-22 01:08:16 -0400

    add view modifer for auto view to current device, and aligned code in gamejs folder

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,15 @@
-//these are the global variables
+//The foolwing is a lsit of global vairables to be used by the game. 
 $(document).ready(function) {
+//this function holds the whole game together
 
-
-//This is a list of PCs for the game (Playable Character)
-
+//This is a list of PCs (Playable Character) for the game 
+//Also, the characters not chosen, become ECs (Enemy character) to be fought by PC
 let characters = [
     'Luke Skywalker': {
         name: 'Luke Skywalker',
         health: 100,
         attack: 5,
-        imageUrl: ,
+        imageUrl: assets/images/LukeSkywalker.jpg,
         enemyCounterAttack: 5
     } 
 
@@ -17,25 +17,28 @@ let characters = [
         name: 'Obi-Wan Kenobi',
         health: 120,
         attack: 8,
-        imageUrl: ,
+        imageUrl: assets/images/ObiWan.jpg,
         enemyCounterAttack: 10
     }
+    
     'Darth Sidious':{
         name: 'Darth Sidious',
         health: 150,
         attack: 12,
-        imageUrl: ,
+        imageUrl: assets/images/DarthSidious.jpg,
         enemyCounterAttack: 15
     }
+
     'Darth Maul': {
         name: 'Darth Maul',
         health: 180,
         attack: 10
-        imageUrl: , 
+        imageUrl: assets/images/DarthMaul.jpg, 
         enemyCounterAttack: 25,
     }
 ];
 
+//creating a function to have the PC battle the EC where the victor continues on to the next round untila ll ECs have been defeated. 
 
 
 
