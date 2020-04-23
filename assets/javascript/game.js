69e5c39124cf74716210f3c8ce3cd36d6e1f37a4
Joshua Balao <joshuacbalao@gmail.com>
2020-04-22 21:38:47 -0400

    remove old pictures and change character names ensure that game start function is correct

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,27 @@
-//The foolwing is a lsit of global vairables to be used by the game. 
+
 
 
 //this function holds the whole game together
-//and resest the game back to beginning
-function (reset) {
-//list of variables for game
 
+$(document).ready(function() {
 
+})
+//list of global variables for game
+let attackbonus =0;
+let player = ; // this will be a holder for the attacker object
+let defender = ; // this will be a holder for the defender object
+let pc = false; //have variable be intially false until chosen
+let ec = false; //have remaining vairables false until PC is chosen
 
 
 //This is a list of PCs (Playable Characters) for the game 
 //Also, the characters not chosen, become ECs (Enemy Characters) to be fought by PC
 let characters = [
-    'Luke Skywalker': {
-        name: 'Luke Skywalker',
+    'Asaj Ventress': {
+        name: 'Asaj Ventress',
         health: 100,
         attack: 5,
-        imageUrl: assets/images/LukeSkywalker.jpg,
+        imageUrl: assets/images/
         enemyCounterAttack: 5
     } 
 
@@ -24,15 +29,15 @@ let characters = [
         name: 'Obi-Wan Kenobi',
         health: 120,
         attack: 8,
-        imageUrl: assets/images/ObiWan.jpg,
+        imageUrl: assets/images/
         enemyCounterAttack: 5
     }
 
-    'Darth Sidious':{
-        name: 'Darth Sidious',
+    'Darth Tyranus':{
+        name: 'Darth Tyranus',
         health: 150,
         attack: 12,
-        imageUrl: assets/images/DarthSidious.jpg,
+        imageUrl: assets/images/
         enemyCounterAttack: 15
     }
 
@@ -40,14 +45,13 @@ let characters = [
         name: 'Darth Maul',
         health: 180,
         attack: 10
-        imageUrl: assets/images/DarthMaul.jpg, 
+        imageUrl: assets/images/
         enemyCounterAttack: 25,
     }
 ];
 
+//
+
+
 //creating a function to have the PC battle the EC where the victor continues on to the next round until all ECs have been defeated. 
 
-
-
-
-}
